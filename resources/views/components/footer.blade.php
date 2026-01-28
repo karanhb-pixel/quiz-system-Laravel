5dcc0f2d0fb33766190e76300008e52c8dd9c3f8
karanhb-pixel <karan.9924304045@gmail.com>
2026-01-28 13:12:36 +0530

    feat: Add a new Blade component for the application footer including navigation links and copyright information.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="bg-gray-800 text-white py-6">
     <div class="container mx-auto text-center">
-        <p>&copy; All Rights Reserved.</p>
+        <p>&copy; {{ date('Y') }} {{ config('app.name', 'Quiz System') }}. All Rights Reserved.</p>
         <div class="m-3 space-x-2">
-            <a href="#" class="hover:underline hover:text-gray-300">Home</a>
-            <a href="#" class="hover:underline hover:text-gray-300">About</a>
-            <a href="#" class="hover:underline hover:text-gray-300">Contact</a>
+            <a href="{{ route('dashboard') }}" class="hover:underline hover:text-gray-300">Home</a>
+            <a href="{{ route('categories.index') }}" class="hover:underline hover:text-gray-300">Categories</a>
+            <a href="{{ route('quizzes.index') }}" class="hover:underline hover:text-gray-300">My Quizzes</a>
         </div>
     </div>
 
